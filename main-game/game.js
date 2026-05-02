3716abd3a36448366bd9997797de77ec85115e41
prashanthans02 <prashanthanand873@gmail.com>
2026-05-02 07:27:39 +0530

    Adding functions to get computer and choice.

--- main-game/game.js
+++ main-game/game.js
@@ -1,1 +1,13 @@
-alert("Hello World!");
+function getComputerChoice(){
+    const availableChoices = ["rock", "paper", "scissors"];
+    const choice = Math.floor(Math.random() * availableChoices.length);
+    return availableChoices[choice];
+}
+
+function getHumanChoice() {
+    let choice = prompt("Enter your choice: ");
+    return choice;
+}
+
+human_choice = getHumanChoice();
+console.log(human_choice);
